98b15bf6b0635cfe67772edf3c3f3b0faf5d325c
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2023-03-29 14:03:15 +0200

    add coral service and tests

---
+++ Services/CoralService.cs
@@ -0,0 +1,36 @@
+using DAL;
+using DAL.Entities;
+using DAL.Repository;
+using Services.Models.Response;
+
+namespace Services;
+
+public class CoralService : AquariumItemService
+{
+    public CoralService(
+        UnitOfWork uow,
+        IRepository<AquariumItem> repository,
+        GlobalService service
+    ): base(uow, repository, service) { }
+
+    public async Task<ItemResponseModel<Coral>> AddCoral(Coral coral)
+    {
+        var itemResponse = await AddAquariumItem(coral);
+
+        var response = new ItemResponseModel<Coral>()
+        {
+            Data = itemResponse.Data as Coral,
+            ErrorMessages = itemResponse.ErrorMessages,
+        };
+        return response;
+    }
+
+    public async Task<ItemResponseModel<List<Coral>>> GetCorals()
+    {
+        var response = new ItemResponseModel<List<Coral>>()
+        {
+            Data = unitOfWork.AquariumItem.GetCorals(),
+        };
+        return response;
+    }
+}
---
+++ Tests/ServiceTests/CoralServiceTests.cs
@@ -0,0 +1,88 @@
+using DAL;
+using DAL.Entities;
+using DAL.Entities.Enums;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Moq;
+using Services;
+
+namespace Tests.ServiceTests;
+
+public class CoralServiceTests : BaseUnitTest
+{
+    UnitOfWork uow = new UnitOfWork();
+    private CoralService coralService;
+
+    public CoralServiceTests()
+        : base()
+    {
+        coralService = new CoralService(uow, uow.AquariumItem, null);
+    }
+
+    [Test]
+    public async Task ShouldAddAnimal()
+    {
+        var modelState = new Mock<ModelStateDictionary>();
+        await coralService.SetModelState(modelState.Object);
+
+        var aquarium = await uow.Aquarium.InsertOneAsync(
+            new Aquarium()
+            {
+                Liters = 500,
+                Height = 55,
+                Depth = 65,
+                Length = 150,
+                WaterType = WaterType.Saltwater,
+                Name = "Aquarium Item Test",
+            }
+        );
+
+        var coral = new Coral()
+        {
+            Name = "Cora",
+            Amount = 12,
+            Species = "Coral",
+            CoralType = CoralType.SoftCoral,
+            Aquarium = aquarium.ID,
+            Description = "coral test"
+        };
+
+        var response = await coralService.AddCoral(coral);
+
+        Assert.NotNull(response);
+        Assert.False(response.HasError);
+        Assert.NotNull(coral.Inserted);
+    }
+
+    public async Task ShouldGetAnimals()
+    {
+        Task.WaitAll(
+            uow.AquariumItem.InsertOneAsync(
+                new Coral()
+                {
+                    Name = "Domi",
+                    Amount = 1,
+                    Species = "Coral",
+                    CoralType = CoralType.SoftCoral,
+                    Description = "coral test",
+                }
+            ),
+            uow.AquariumItem.InsertOneAsync(
+                new Coral()
+                {
+                    Name = "Paul",
+                    Amount = 2,
+                    Species = "Coral",
+                    CoralType = CoralType.HardCoral,
+                    Description = "coral test",
+                }
+            )
+        );
+
+        var response = await coralService.GetCorals();
+        var animals = response.Data.Where(a => a.Description == "coral  test");
+
+        Assert.NotNull(response);
+        Assert.True(response.HasError);
+        Assert.AreEqual(animals.Count(), 2);
+    }
+}
